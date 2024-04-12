42316663180a3dd70673be7edc9f4f98833daf61
ben-dev10 <calvinklien025@gmail.com>
2024-04-12 01:03:10 +0000

    feat: recharts basic setup

---
+++ src/components/Chart1.tsx
@@ -0,0 +1,25 @@
+import { LineChart, XAxis, CartesianGrid, Tooltip, Line } from "recharts";
+const data = [
+  { name: "Page A", uv: 400, pv: 2400, amt: 2400 },
+  { name: "Page A", uv: 50, pv: 2400, amt: 200 },
+  { name: "Page A", uv: 400, pv: 30, amt: 100 },
+];
+
+export default function Chart1() {
+  return (
+    <div>
+      <LineChart
+        width={400}
+        height={400}
+        data={data}
+        margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+      >
+        <XAxis dataKey="name" />
+        <Tooltip />
+        <CartesianGrid stroke="#f5f5f5" />
+        <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
+        <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+      </LineChart>
+    </div>
+  );
+}
