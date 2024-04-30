13484e83c23f1de21e493127ae7ef698a2e343d1
ben-dev10 <calvinklien025@gmail.com>
2024-04-30 00:42:25 +0000

    feat: added vercel-analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import AnalyticsCard from "./components/AnalyticsCard";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="p-5">
       <AnalyticsCard />
+      <Analytics />
     </div>
   );
 }
